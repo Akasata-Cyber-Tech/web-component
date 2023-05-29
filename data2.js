8bd6419c5ede890273692dc295ad3fcf2aed1d3c
TFSID <93431570+TFSID@users.noreply.github.com>
2023-05-29 16:59:08 +0700

    Update DataTables

---
+++ data2.js
@@ -0,0 +1,74 @@
+var dataJSON ={
+    "Count": 10,
+    "Filtered": 10,
+    "List": [
+      {
+        "CreateBy": "Admin                                             ",
+        "CreateDate": "2023-05-29T00:00:00",
+        "Id": "e1f6f36a-f674-44a6-9018-1a2cf72bc33c",
+        "Password": "",
+        "Url": "",
+        "Username": ""
+      },
+      {
+        "CreateBy": "Admin                                             ",
+        "CreateDate": "2023-05-29T00:00:00",
+        "Id": "246fe7f5-b9a2-4a7c-b351-2ff6c8853c62",
+        "Password": "password",
+        "Url": "https://siap.bankmandiritaspen.co.id/Account/Login",
+        "Username": "jotaro"
+      },
+      {
+        "CreateBy": "Admin                                             ",
+        "CreateDate": "2023-05-29T00:00:00",
+        "Id": "47c855d7-6dc7-47df-ae31-4e07ab408fda",
+        "Password": "",
+        "Url": "",
+        "Username": ""
+      },
+      {
+        "CreateBy": "Admin                                             ",
+        "CreateDate": "2023-05-29T00:00:00",
+        "Id": "33cb1249-35aa-4cb6-bd53-5e812f0aad30",
+        "Password": "",
+        "Url": "",
+        "Username": ""
+      },
+      {
+        "CreateBy": "Admin                                             ",
+        "CreateDate": "2023-05-29T00:00:00",
+        "Id": "ded9c370-2efe-45a0-9581-9898c030606d",
+        "Password": "password",
+        "Url": "https://siap.bankmandiritaspen.co.id/Account/Login",
+        "Username": "jotaro"
+      },
+      {
+        "CreateBy": "Admin                                             ",
+        "CreateDate": "2023-05-29T00:00:00",
+        "Id": "6368e0c9-6b6f-4582-a0f2-9c861f3510e3",
+        "Password": "password",
+        "Url": "http://localhost:58768/Account/Login",
+        "Username": "admin"
+      },
+      {
+        "CreateBy": "Admin                                             ",
+        "CreateDate": "2023-05-29T00:00:00",
+        "Id": "f4344c22-5cd6-40b0-ab47-d4a665af1e69",
+        "Password": "password",
+        "Url": "http://localhost:58768/Account/Login",
+        "Username": "admin"
+      },
+      {
+        "CreateBy": "Admin                                             ",
+        "CreateDate": "2023-05-29T00:00:00",
+        "Id": "b9e7ef79-6b8b-4ca1-8dc6-f8295bea9b62",
+        "Password": "",
+        "Url": "",
+        "Username": ""
+      }
+    ],
+    "Code": 200,
+    "Succeeded": true,
+    "Message": "All OK",
+    "Description": ""
+  }
