a4faf6d4ed6d55095e97addcda582b8b80b5092d
TFSID <tegar.firman215@gmail.com>
2023-05-31 16:40:07 +0700

    Layout Experimenting Using .NET MVC
    Experimenting Making A Layout Using .Net With MVC Method

---
+++ Experiment/NetCoreExperiment/NetCore/AniMae/AniMae/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AKASATA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
